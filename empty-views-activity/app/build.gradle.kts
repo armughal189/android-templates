667443e8f06527b022834957d6ca3284204965a8
armughal189 <samughal189@gmail.com>
2026-02-07 21:24:22 +0500

    Added Empty Views Activity Template

---
+++ empty-views-activity/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+  alias(libs.plugins.android.application)
+}
+
+android {
+  namespace = "io.armughal189.empty_views_activity"
+  compileSdk {
+    version = release(36)
+  }
+
+  defaultConfig {
+    applicationId = "io.armughal189.empty_views_activity"
+    minSdk = 28
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0.0"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+}
+
+dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.material)
+  implementation(libs.androidx.activity)
+}
---
+++ empty-views-activity/app/src/main/java/io/armughal189/empty_views_activity/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.armughal189.empty_views_activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
